43db65be8cd16e75317b994ead438e7ef7ea322d
pnadalini <pietro.nadalini@hotmail.com>
2018-08-29 00:03:58 -0600

    Added second functionality

--- js/actions.js
+++ js/actions.js
@@ -1,4 +1,11 @@
-function switchTheme (){
-  document.getElementById('webPageTheme').href = document.getElementById('webPageTheme').href.includes("css/bootstrap.min.css") ?
-    document.getElementById('webPageTheme').href = "./css/dark.bootstrap.min.css" : document.getElementById('webPageTheme').href = "./css/bootstrap.min.css";
+function switchTheme() {
+ document.getElementById('webPageTheme').href = document.getElementById('webPageTheme').href.includes("css/bootstrap.min.css") ?
+  document.getElementById('webPageTheme').href = "./css/dark.bootstrap.min.css" : document.getElementById('webPageTheme').href = "./css/bootstrap.min.css";
+}
+
+function changeFont() {
+ if (document.getElementsByTagName("main")[0].classList.contains('arial-black'))
+  document.getElementsByTagName("main")[0].classList.remove("arial-black");
+ else
+  document.getElementsByTagName("main")[0].classList.add("arial-black");
 }
