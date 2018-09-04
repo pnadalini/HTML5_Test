a7241a4fa4c8642d2a8dc6200d8b7023947efdbd
pnadalini <pietro.nadalini@hotmail.com>
2018-09-04 01:03:33 -0600

    Added all style-change functionalities

--- js/actions.js
+++ js/actions.js
@@ -1,11 +1,25 @@
 function switchTheme() {
- document.getElementById('webPageTheme').href = document.getElementById('webPageTheme').href.includes("css/bootstrap.min.css") ?
-  document.getElementById('webPageTheme').href = "./css/dark.bootstrap.min.css" : document.getElementById('webPageTheme').href = "./css/bootstrap.min.css";
+  document.getElementById('webPageTheme').href = document.getElementById('webPageTheme').href.includes("css/bootstrap.min.css") ?
+    document.getElementById('webPageTheme').href = "./css/dark.bootstrap.min.css" : document.getElementById('webPageTheme').href = "./css/bootstrap.min.css";
 }
 
 function changeFont() {
- if (document.getElementsByTagName("main")[0].classList.contains('arial-black'))
-  document.getElementsByTagName("main")[0].classList.remove("arial-black");
- else
-  document.getElementsByTagName("main")[0].classList.add("arial-black");
+  document.querySelector("main").classList.toggle('arial-black');
+}
+
+function changeNavColor() {
+  document.querySelector("nav").classList.toggle('navbar-dark');
+  document.querySelector("nav").classList.toggle("bg-dark");
+  document.querySelector("nav").classList.toggle('navbar-light');
+  document.querySelector("nav").classList.toggle("bg-light");
+}
+
+function changeFontColor(){
+  document.querySelectorAll("main").forEach((element) => {
+    element.classList.toggle("text-success");
+  });
+}
+
+function toggleFooter(){
+  document.querySelector(".footer").classList.toggle("position-fixed");
 }
